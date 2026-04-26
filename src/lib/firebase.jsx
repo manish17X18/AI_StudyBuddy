97c7248ebab883ac2fd945c4334a656cf552d3c9
manish17x18 <manishdesh1710@gmail.com>
2026-04-26 15:42:57 +0530

    final change

--- src/lib/firebase.jsx
+++ src/lib/firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // 1. Add this import
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCs-7RBIsavD0MsUPKBuXLjGzwnaI6S4ug", 
+  apiKey: "Mykey", 
   authDomain: "study-buddy-21758.firebaseapp.com",
   projectId: "study-buddy-21758",
   storageBucket: "study-buddy-21758.firebasestorage.app",
-  messagingSenderId: "71734315557",
-  appId: "1:71734315557:web:dad227df6c203d2a28cdcc"
+  messagingSenderId: "123456789",
+  appId: "123456789"
 };
 
 // Initialize Firebase
